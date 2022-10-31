e0d3c926c7cafa9d2a0005ad12954edab1103626
Sanin Oleksandr <saninalexsanin@gmail.com>
2022-10-31 17:09:58 +0200

    Закінчив секцію teams (mobile)

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,7 +3,6 @@
     const desktopMenu = document.querySelector('.desktop-menu');
     const openMenuBtn = document.querySelector('.mobile-btn--open');
     const closeMenuBtn = document.querySelector('.mobile-btn--close');
-    const additionalInfo = document.querySelector('.works');
 
   
     const toggleMenu = () => {
@@ -28,7 +27,6 @@
     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
       if (!e.matches) return;
       
-      additionalInfo.classList.remove('is-hidden');
       mobileMenu.classList.remove('is-open');
       desktopMenu.classList.remove('is-hidden');
       openMenuBtn.classList.toggle('is-hidden');
@@ -39,7 +37,7 @@
       if (!e.matches) return;
           desktopMenu.classList.toggle('is-hidden');
           openMenuBtn.classList.remove('is-hidden');
-          additionalInfo.classList.toggle('is-hidden');
+          
           
     });
   })();
